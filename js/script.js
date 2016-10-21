4de15e2630c21b88a36db20e4d9b97a9d4260855
djacquel <github@pygmeeweb.com>
2016-10-21 17:25:21 +0200

    analytics tracking

--- js/script.js
+++ js/script.js
@@ -26,8 +26,7 @@ $(function() {
   }
 });
 
-
-{% if site.google_analytics_tracking_id and site.google_analytics_tracking_id != "" %}
+{% if jekyll.environment == 'production' and site.google_analytics %}
   // GOOGLE ANALYTICS CODE
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
